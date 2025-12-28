5d9426f4d8945f31143afeb0dfe84544fd0e599a
SockolValera <148217276+SockolValera@users.noreply.github.com>
2025-12-28 23:13:37 +0200

    Update WalletArbitraries.cs

--- Tests/WalletArbitraries.cs
+++ Tests/WalletArbitraries.cs
@@ -1,11 +1,5 @@
-﻿using FsCheck;
-using FsCheck.FSharp;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FsCheck;
+using FsCheck.Fluent;
 using WalletPropertyTesting.Domain;
 using WalletPropertyTesting.Tests.Arbitraries;
 
@@ -13,15 +7,33 @@ namespace WalletPropertyTesting.Tests
 {
     public static class WalletArbitraries
     {
-        public static Arbitrary<Money> Money() =>
-            throw new NotImplementedException();
+        public static Arbitrary<Money> Money() => MoneyGenerator.Money();
+        public static Arbitrary<MoneyPair> MoneyPairForSubtraction() => MoneyGenerator.MoneyPairForSubtraction();
+        public static Arbitrary<MoneyInsufficientPair> MoneyPairInsufficient() => MoneyGenerator.MoneyPairInsufficient();
+        public static Arbitrary<Transaction> Transaction() => TransactionGenerator.Transaction();
+        public static Arbitrary<WalletOperation> WalletOperation() => OperationGenerator.Operation();
+        public static Arbitrary<ValidOperationSequence> ValidOperationSequence() => OperationGenerator.ValidOperationSequence();
 
+        public static Arbitrary<Wallet> Wallet()
+        {
+            var gen =
+                from seq in ValidOperationSequence().Generator
+                select BuildWallet(seq);
 
+            return Arb.From(gen);
+        }
 
-
-
-        public static Arbitrary<WalletOperation> WalletOperation() =>
-            throw new NotImplementedException();
+        private static Wallet BuildWallet(ValidOperationSequence seq)
+        {
+            var w = new Wallet();
+            foreach (var op in seq.Ops)
+            {
+                if (op.Type == WalletOpType.Deposit)
+                    w.Deposit(new Money(op.Amount));
+                else
+                    w.Withdraw(new Money(op.Amount));
+            }
+            return w;
+        }
     }
-
 }
