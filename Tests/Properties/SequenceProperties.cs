37481589c9f966e4080da64c82c983a47d40caed
SockolValera <148217276+SockolValera@users.noreply.github.com>
2025-12-28 23:14:43 +0200

    Update SequenceProperties.cs

--- Tests/Properties/SequenceProperties.cs
+++ Tests/Properties/SequenceProperties.cs
@@ -1,19 +1,59 @@
-﻿using FsCheck.Xunit;
-using System;
-using System.Collections.Generic;
+using FsCheck.Xunit;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WalletPropertyTesting.Domain;
 using WalletPropertyTesting.Tests.Arbitraries;
+using FsCheck;
+using FsCheck.Fluent;
+using FsCheck.Xunit;
+
 
 namespace WalletPropertyTesting.Tests.Properties
 {
-
     public class SequenceProperties
     {
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public bool WalletService_ApplyingValidSequence_DoesNotThrow_AndBalanceMatches(ValidOperationSequence seq)
+        {
+            var repo = new InMemoryWalletRepository();
+            var service = new WalletService(repo);
 
+            var wallet = service.CreateWallet();
 
+            foreach (var op in seq.Ops)
+            {
+                if (op.Type == WalletOpType.Deposit)
+                    service.Deposit(wallet.Id, op.Amount);
+                else
+                    service.Withdraw(wallet.Id, op.Amount);
+            }
+
+            var fromService = service.GetBalance(wallet.Id).Amount;
+            var fromRepo = repo.Get(wallet.Id).Balance.Amount;
+
+            return fromService == fromRepo
+                   && fromService >= 0m;
+        }
+
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public bool WalletService_BalanceEqualsNetOperations(ValidOperationSequence seq)
+        {
+            var repo = new InMemoryWalletRepository();
+            var service = new WalletService(repo);
+
+            var wallet = service.CreateWallet();
+
+            foreach (var op in seq.Ops)
+            {
+                if (op.Type == WalletOpType.Deposit)
+                    service.Deposit(wallet.Id, op.Amount);
+                else
+                    service.Withdraw(wallet.Id, op.Amount);
+            }
+
+            var deposits = seq.Ops.Where(o => o.Type == WalletOpType.Deposit).Sum(o => o.Amount);
+            var withdraws = seq.Ops.Where(o => o.Type == WalletOpType.Withdraw).Sum(o => o.Amount);
+
+            return service.GetBalance(wallet.Id).Amount == deposits - withdraws;
+        }
     }
-
 }
