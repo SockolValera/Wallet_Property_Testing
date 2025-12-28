5bc47db99429ffb00b1ebc5252c3c5fcab9a438d
SockolValera <148217276+SockolValera@users.noreply.github.com>
2025-12-28 23:14:29 +0200

    Update RepositoryProperties.cs

--- Tests/Properties/RepositoryProperties.cs
+++ Tests/Properties/RepositoryProperties.cs
@@ -1,17 +1,54 @@
-﻿using FsCheck.Xunit;
+using FsCheck.Xunit;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WalletPropertyTesting.Domain;
+using WalletPropertyTesting.Tests.Arbitraries;
+using FsCheck;
+using FsCheck.Fluent;
+using FsCheck.Xunit;
+
 
 namespace WalletPropertyTesting.Tests.Properties
 {
-
     public class RepositoryProperties
     {
-       
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public bool Repository_SaveThenGet_PreservesIdAndBalance(Wallet wallet)
+        {
+            var repo = new InMemoryWalletRepository();
+            repo.Save(wallet);
 
+            var loaded = repo.Get(wallet.Id);
+
+            return loaded.Id == wallet.Id
+                   && loaded.Balance.Amount == wallet.Balance.Amount
+                   && loaded.History.Count == wallet.History.Count;
+        }
+
+        [Property]
+        public bool Repository_GetUnknown_Throws()
+        {
+            var repo = new InMemoryWalletRepository();
+            try
+            {
+                repo.Get(Guid.NewGuid());
+                return false;
+            }
+            catch (KeyNotFoundException)
+            {
+                return true;
+            }
+        }
+
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public bool Repository_All_ContainsSavedWallets(Wallet a, Wallet b)
+        {
+            var repo = new InMemoryWalletRepository();
+            repo.Save(a);
+            repo.Save(b);
+
+            var all = repo.All().Select(x => x.Id).ToHashSet();
+            return all.Contains(a.Id) && all.Contains(b.Id);
+        }
     }
 }
